dc6a2b3ecee6477cda7b2633bf11c56a787f3573
Brandon Lin <brandonlin9716@gmail.com>
2024-12-27 23:41:39 -0800

    Increment score when a new gif is clicked

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,8 +1,8 @@
 export { Card };
 
-const Card = ({url, title}) => {
+const Card = ({url, title, onClick}) => {
   return (
-    <div className="card">
+    <div className="card" onClick={onClick}>
       <img src={url} alt={title} />
       <h3>{title}</h3>
     </div>
--- src/components/Memory.jsx
+++ src/components/Memory.jsx
@@ -8,21 +8,37 @@ const Memory = () => {
   const NUM_CARDS = 12;
 
   const [gifs, setGifs] = useState([]);
+  const [clickedGifs, setClickedGifs] = useState(new Set([]));
 
   useEffect(() => {
-    searchGifs(THEME, NUM_CARDS)
-      .then((data) => setGifs(data));
+    searchGifs(THEME, NUM_CARDS).then((data) => setGifs(data));
   }, []);
+
+  const handleCardClick = (url) => {
+    if (clickedGifs.has(url)) {
+      // user has clicked this gif (reset score to 0)
+      setClickedGifs(new Set([]));
+    } else {
+      setClickedGifs(new Set([...clickedGifs, url]));
+    }
+  };
 
   return (
     <>
       <div>
-        <div>Score: </div>
+        <div>Score: {clickedGifs.size}</div>
         <div>High Score: </div>
       </div>
       <section className="card-holder">
         {gifs.map((gif) => {
-          return <Card url={gif.url} title={gif.title} key={gif.url} />;
+          return (
+            <Card
+              url={gif.url}
+              title={gif.title}
+              key={gif.url}
+              onClick={() => handleCardClick(gif.url)}
+            />
+          );
         })}
       </section>
     </>
