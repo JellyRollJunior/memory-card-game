dc66e4f14c161356d94e129f6771589f5d9b6542
Brandon Lin <brandonlin9716@gmail.com>
2024-12-28 15:46:26 -0800

    Display win message on win

--- src/components/Memory.jsx
+++ src/components/Memory.jsx
@@ -7,7 +7,7 @@ export { Memory };
 
 const Memory = () => {
   const THEME = 'sailor moon';
-  const NUM_CARDS = 12;
+  const NUM_CARDS = 3;
 
   const [gifs, setGifs] = useState([]);
   const [clickedGifs, setClickedGifs] = useState(new Set([]));
@@ -28,19 +28,23 @@ const Memory = () => {
     setGifs(array);
   };
 
+  const displayWin = () => {
+    document.querySelector('.win-modal').classList.add('visible');
+  };
+
   const handleCardClick = (url) => {
     if (clickedGifs.has(url)) {
       // user has clicked this gif (reset score to 0)
       setClickedGifs(new Set([]));
     } else {
-      // increment score
+      // user has clicked a new gif - increment score
       setClickedGifs(new Set([...clickedGifs, url]));
       if (clickedGifs.size >= highScore) {
         setHighScore(clickedGifs.size + 1);
       }
       // if user score == NUM_CARDS, show victory message
       if (clickedGifs.size + 1 >= NUM_CARDS) {
-        alert('You win!');
+        displayWin();
       }
     }
     shuffleGifs();
@@ -48,13 +52,22 @@ const Memory = () => {
 
   return (
     <>
+      <div className="win-modal">
+        <div className='win-card'>
+          <img src={prettyGuardianLogo} alt="" />
+          <div>Congrats you win!</div>
+        </div>
+      </div>
       <header>
         <img src={prettyGuardianLogo} alt="" />
       </header>
       <main>
         <div className="wrapper">
           <h1>Sailor Moon Memory</h1>
-          <h3>Get points by clicking on an image but don&apos;t click on any more than once!</h3>
+          <h3>
+            Get points by clicking on an image but don&apos;t click on any more
+            than once!
+          </h3>
           <div className="score">
             <h2>High Score: {highScore}</h2>
             <h2>Score: {clickedGifs.size}</h2>
